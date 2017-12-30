c4228246cb10a6d83946cb7da2dbc0d60cf32307
zhaozimu <zhaozimu@jd.com>
2017-12-30 15:10:03 +0800

    测试接口-nginx跨域携带cookie

--- src/main/java/api/controller/LoginController.java
+++ src/main/java/api/controller/LoginController.java
@@ -9,7 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("login")
@@ -25,8 +31,31 @@ public class LoginController {
         //UserService.login()方法中已对user进行判断，如不能正确的返回user则抛出异常
         User user = service.login(username, password);
 //        user.setPassword(null);
-        SessionUtil.setUser(request , user);
+        SessionUtil.setUser(request, user);
         return user;
+    }
+
+    /**
+     * 测试接口：nginx跨域携带cookie
+     *
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    @RequestMapping("test")
+    public void Test(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                cookie.getName();
+                cookie.getValue();
+                response.getWriter().print(cookie.getName() + ": " + cookie.getValue());
+            }
+        } else {
+            Cookie cookie = new Cookie("ZIMU", "testNginx");
+            response.addCookie(cookie);
+            response.getWriter().print("First request: Cookie writing successful~");
+        }
     }
 
 }
